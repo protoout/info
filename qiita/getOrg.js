d74564f44a9534cac0864811ddc2888356e4aa74
n0bisuke <rsksound@gmail.com>
2021-03-09 00:44:59 +0900

    qiita

--- qiita/getOrg.js
+++ qiita/getOrg.js
@@ -6,13 +6,30 @@ module.exports = async (MAGAZINE_KEY) => {
         const QIITA_ORG = `https://qiita.com/organizations/protoout-studio/activities.atom`;
 
         const res = await axios.get(QIITA_ORG);
-        // console.log(res.data);
 
         const options = { ignoreComment: true, alwaysChildren: true };
         const result = convert.xml2json([res.data], options);
-        // console.log(result);
-        let output = result;
-        
+        const json = JSON.parse(result).elements[0].elements;
+
+        let output = [];
+
+        json.map(elem => elem.elements)
+            .map(elem => {
+                const obj = {};
+                for (let i = 0, len = elem.length; i < len; i++) {
+                    const item = elem[i];
+                    if(item.name === 'id')obj.id = item.elements[0].text;
+                    if(item.name === 'updated')obj.updated = item.elements[0].text;
+                    if(item.name === 'url')obj.url = item.elements[0].text;
+                    if(item.name === 'title')obj.title = item.elements[0].text;
+                    if(item.name === 'published')obj.published = item.elements[0].text;
+                    if(item.name === 'author')obj.author = item.elements[0].elements[0].text;
+                    if(item.name === 'link')obj.link = item.attributes.href;
+                    //if(item.name === 'content')obj.content = item.elements[0].text;
+                }
+                if(Object.keys(obj).length) output.push(obj);
+            })
+            
         return output;
     } catch (error) {
         throw new Error(error);
