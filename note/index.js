4b633e1e62055004bb9074f3fa98896f5c779cc6
n0bisuke <rsksound@gmail.com>
2021-03-08 18:32:40 +0900

    zentai

--- note/index.js
+++ note/index.js
@@ -1,15 +1,15 @@
 
 const fs = require(`fs`);
-const getInterview = require(`./interview`);
+const getMagazine = require(`./getMagazine`);
 
-let output = {};
 const MAGAZINE_KEY_INTERVIEW = `m27cd63996372`; //プロトアウトインタビューマガジン
 const MAGAZINE_KEY_ZENTAI = `m03f109eaf0a8`; //プロトアウト学生全体マガジン
 
 (async ()=>{
-    const interview = await getInterview(MAGAZINE_KEY_INTERVIEW);
-    const zentai = await getInterview(MAGAZINE_KEY_ZENTAI);
-    output = {
+    const interview = await getMagazine(MAGAZINE_KEY_INTERVIEW);
+    const zentai = await getMagazine(MAGAZINE_KEY_ZENTAI);
+
+    const output = {
         interview: interview,
         zentai: zentai
     }
