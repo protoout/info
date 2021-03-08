ed68a8919ab3aae1092d900a50dca47cc3a21bb4
n0bisuke <rsksound@gmail.com>
2021-03-09 04:49:22 +0900

    news

---
+++ codezine/getMagazine.js
@@ -0,0 +1,24 @@
+const axios = require(`axios`);
+
+module.exports = async (URL) => {    
+    try {
+        const res = await axios.get(URL);
+        const html = res.data;
+        const items = html.replace(/\r?\n/g,"").match(/<li id="(.*?)class="tag"/g);
+
+        const output = items.map(elem => {
+            let result = {};
+            result.id = elem.match(/<li id="(.*?)">/)[1];
+            result.date = elem.match(/class="day">(.*?)<\/div>/)[1];
+            const regexp = new RegExp(`<h2><a href="\/article\/detail\/${result.id}">(.*?)<\/a><\/h2>`);
+            result.title = elem.match(regexp)[1];
+
+            result.image = `https:`+elem.match(/src="(.*?)"/)[1];
+            result.content = elem.match(/<p>(.*?)<\/p>/)[1];
+            return result;
+        })
+        return output;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
---
+++ codezine/index.js
@@ -0,0 +1,15 @@
+
+const fs = require(`fs`);
+const getMagazine = require(`./getMagazine`);
+
+const MIJIKA_RENSAI = `https://codezine.jp/article/corner/842`; //「自分自身で課題を解決するDX事例紹介」連載一覧
+
+(async ()=>{
+    const mijika = await getMagazine(MIJIKA_RENSAI);
+
+    const output = {
+        mijika: mijika
+    }
+
+    fs.writeFileSync('./docs/codezine.json', JSON.stringify(output));
+})();
